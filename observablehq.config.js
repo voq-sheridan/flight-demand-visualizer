5f30f8b8bae237de01002b07130b04643c8c92dc
Minh <voq@sheridancollege.ca>
2026-03-29 01:35:37 -0400

    test 46

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,5 +1,5 @@
 export default {
   title: "Flight Demand Visualizer",
-  head: '<link rel="icon" href="./favicon.svg" type="image/svg+xml">',
+  head: '<link rel="icon" href="./favicon.svg" type="image/svg+xml"><link rel="shortcut icon" href="./favicon.ico" type="image/x-icon">',
   root: "src",
 };
